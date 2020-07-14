27099e5b229f63af0c838d95cd7e9f6596976e13
SCOOP <SCOOP@Carlos>
2020-07-14 17:09:28 -0500

    Primera versión

---
+++ PaginaWebTiendaVirtual/src/main/java/net/osgg/PaginaWebTiendaVirtual/PaginaWebTiendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package net.osgg.PaginaWebTiendaVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebTiendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebTiendaVirtualApplication.class, args);
+	}
+
+}
---
+++ PaginaWebTiendaVirtual/src/test/java/net/osgg/PaginaWebTiendaVirtual/PaginaWebTiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package net.osgg.PaginaWebTiendaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebTiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
