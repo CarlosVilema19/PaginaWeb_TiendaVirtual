d6168e450ac705567e6ca3771e505a18cd164c10
SCOOP <SCOOP@Carlos>
2020-07-15 16:47:50 -0500

    Codificar CustomWeb

--- PaginaWebTiendaVirtual/src/main/java/net/osgg/PaginaWebTiendaVirtual/config/CustomWebMvcAutoConfig.java
+++ PaginaWebTiendaVirtual/src/main/java/net/osgg/PaginaWebTiendaVirtual/config/CustomWebMvcAutoConfig.java
@@ -1,5 +1,22 @@
 package net.osgg.PaginaWebTiendaVirtual.config;
 
-public class CustomWebMvcAutoConfig {
+import org.springframework.beans.factory.annotation. Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.servlet. DispatcherServletAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config. annotation. ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class CustomWebMvcAutoConfig implements WebMvcConfigurer {
+
+@Value("${upload.path}")
+public String uploadDir;
+
+@Override
+public void addResourceHandlers (ResourceHandlerRegistry registry) {
+String myExternalFilePath = "file:"+uploadDir+"/";
+registry.addResourceHandler ("/pic/**").addResourceLocations(myExternalFilePath);
+}
 }
